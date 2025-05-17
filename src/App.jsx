8fc31ae5e69cdeda7907c37d3788ed7429ae405c
PabloVS044 <pvasquezs044@gmail.com>
2025-05-17 16:23:30 -0600

    Refactor App component: reorganize imports, enhance routing, and integrate new event and publication creation pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import LandingPage from './pages/LandingPage'
-import EventsPage from './pages/Dashboard'
+import { setUpInterceptors } from "@/lib/auth.js"
+import { AuthProvider, useAuth } from "@/providers/AuthProvider.js"
+import { useEffect } from "react"
+import { Route, BrowserRouter as Router, Routes, useNavigate } from 'react-router-dom'
+import { Toaster } from "sonner"
+import DashboardLayout from "./components/dashboardLayout"
+import { ThemeProvider } from "./components/theme-provider"
 import Login from './pages/auth/login/Login.jsx'
 import Register from './pages/auth/signup/Register.jsx'
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom'
-import DashboardPage from "./pages/dashboard/DashboardPage";
-import ReportsPage from "./pages/dashboard/ReportsPage";
-import MapsPage from "./pages/dashboard/MapsPage";
-import NewReportPage from "./pages/dashboard/NewReportPage";
-import PublicationsPage from "./pages/dashboard/PublicationsPage";
-import DashboardLayout from "./components/dashboardLayout";
-import { ThemeProvider } from "./components/theme-provider";
-import { Toaster } from "sonner";
-import { AuthProvider } from "@/providers/AuthProvider.js";
-import { useEffect } from "react";
-import { setUpInterceptors } from "@/lib/auth.js";
+import DashboardPage from "./pages/dashboard/DashboardPage"
+import EventsPage from "./pages/dashboard/EventsPage"
+import MapsPage from "./pages/dashboard/MapsPage"
+import NewReportPage from "./pages/dashboard/NewReportPage"
+import PublicationsPage from "./pages/dashboard/PublicationsPage"
+import CreatePublication from "./pages/publication/CreatePublication.jsx"
+import ReportsPage from "./pages/dashboard/ReportsPage"
+import CreateEvent from "./pages/event/CreateEvent.jsx"
+import LandingPage from './pages/LandingPage'
 import MapaIA from "./pages/ai-map/MapaIA";
 import EventosUser from "./pages/feed/EventosFeedPage.jsx";
 import PublicacionesFeedPage from './pages/feed/PublicacionesFeedPage.jsx'
@@ -34,9 +36,12 @@ function App() {
 
 function RoutesWrapper() {
   const navigate = useNavigate()
+  const {
+    refreshToken
+  } = useAuth();
 
   useEffect(() => {
-    setUpInterceptors(navigate)
+    setUpInterceptors(navigate, refreshToken)
   }, []);
 
   return (
@@ -44,18 +49,20 @@ function RoutesWrapper() {
       <Route path="/" element={<LandingPage/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/register" element={<Register/>}/>
-      <Route path="/mapa-ia" element={<MapaIA />} />
       <Route path="/dashboard" element={<DashboardLayout/>}>
         <Route index element={<DashboardPage/>}/>
         <Route path="maps" element={<MapsPage/>}/>
         <Route path="reports" element={<ReportsPage/>}/>
         <Route path="reports/new" element={<NewReportPage/>}/>
         <Route path="events" element={<EventsPage/>}/>
+        <Route path="events/new" element={<CreateEvent/>}/>
         <Route path="publications" element={<PublicationsPage/>}/>
+        <Route path="publications/new" element={<CreatePublication/>}/>
+        <Route path="/events" element={<EventosUser/>}/>
+        <Route path="/publications" element={<PublicacionesFeedPage/>}/>
+        <Route path="/reports" element={<ReportesFeedPage/>}/>
+        <Route path="/mapa-ia" element={<MapaIA />} />
       </Route>
-      <Route path="/events" element={<EventosUser/>}/>
-      <Route path="/publications" element={<PublicacionesFeedPage/>}/>
-      <Route path="/reports" element={<ReportesFeedPage/>}/>
     </Routes>
   )
 
